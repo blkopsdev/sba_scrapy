a73c4076aafd65ab06c4902f5e576e03e487de21
blkopsdev <blkopsdev@gmail.com>
2019-02-03 22:30:30 -0500

    profiles table

--- sba/spiders/sba.py
+++ sba/spiders/sba.py
@@ -102,14 +102,6 @@ class SbaSpider(scrapy.Spider):
                     response.meta.update({
                         'economic_id': economic_id
                     })
-                    #     try:
-                    #         self.cursor.execute(
-                    #             """UPDATE economic_group SET num_of_firms = %s WHERE id = $d""", (number_of_firms, economic_id,)
-                    #         )
-                    #         self.conn.commit()
-                    #     except MySQLdb.Error, e:
-                    #         print("Error %d: %s" % (e.args[0], e.args[1]))
-
                 except:
                     print "Error: unable to fecth economic group data"
 
@@ -197,7 +189,25 @@ class SbaSpider(scrapy.Spider):
     def get_data(self, response):
 
         keywords = response.xpath('//h3[contains(text(), "Keywords")]/following-sibling::div[@class="indent_same_as_profilehead"]/text()').extract_first()
-        naics_codes = response.xpath('//table[@summary="NAICS Codes"]//tr//td[contains(@headers, "C2")]/text()').extract()
+        office = response.xpath('//h3[contains(text(), "Business Development Servicing Office")]/following-sibling::div[@class="indent_same_as_profilehead"]/text()').extract_first()
+
+        # naics_codes = response.xpath('//table[@summary="NAICS Codes"]//tr//td[contains(@headers, "C2")]/text()').extract()
+        ids = response.xpath(
+            '//table[@summary="NAICS Codes"]//tr//th[@id and text()]/@id'
+        ).extract()
+        keys = response.xpath(
+            '//table[@summary="NAICS Codes"]//tr//th[@id and text()]/text()'
+        ).extract()
+        trs = response.xpath(
+            '//table[@summary="NAICS Codes"]//tbody//tr'
+        )
+        naics_data = None
+        for tr in trs:
+            datum = {}
+            for idx, id in enumerate(ids):
+                value = tr.xpath('.//td[contains(@headers, "{}")]/text()'.format(id)).extract_first()
+                datum[keys[idx]] = value
+                naics_data = json.dumps(datum)
 
         performance_blocks = response.xpath('//div[@class="referencebox"]')
         performances = []
@@ -214,21 +224,13 @@ class SbaSpider(scrapy.Spider):
                    performance[key.replace(':', '')] = value
 
             performances.append(performance)
-
             performance_json = json.dumps(performances)
 
         info = {
-            'econ': response.meta.get('econmic_key'),
-            'number_of_firms': response.meta.get('number_of_firms'),
-            'profile_table': response.meta.get('profile_table'),
-            'naics': response.meta.get('naics', '').encode('utf-8'),
-            'state_code': response.meta.get('State', '').encode('utf-8'),
             'keywords': keywords.encode('utf-8'),
-            'naics_codes': ','.join(naics_codes).encode('utf-8'),
-            'performances': performance_json
         }
 
-        data = response.xpath('//div[@data-role="collapsible-set"]//div[@class="profileline" and position() != 1 and position() != 2]')
+        data = response.xpath('//div[@class="profileline" and position() != 1]')
         for datum in data:
             key = datum.xpath('.//div[@class="profilehead"]//text()').extract_first()
             key = key.strip() if key else None
@@ -239,5 +241,71 @@ class SbaSpider(scrapy.Spider):
             if not value:
                 value = ''
             info[key] = value.encode('utf-8')
-
+        try:
+            list_id = response.meta.get('list_id')
+            self.cursor.execute("""SELECT id FROM profiles WHERE email = %s AND list_id = %s"""),(info['Email Address:'], list_id,)
+            profile = self.cursor.fetchall()
+            if not profile:
+                try:
+                    self.cursor.execute(
+                        """INSERT INTO profiles (user_id, name_of_firm, trade_name, duns_num, p_dunms_num, address1, address2, \
+                        city, state, zip, phone, fax, email, www_page, ecom_website, county_code, cong_district, ms_area, cage_code,\
+                        established_year, gsa_contact, ownership, sba_8a_num, sba_8a_ent, sba_8a_exit,\
+                        ishubzone_cert, hubsone_certdate, 8a_jv_ent, 8a_jv_exit, naics_table, keywords, performance_history, list_id\
+                        quality_assurance, electronic_data, export_business_activity, exporting_to, bonding_agg, bonding_cont, \
+                        con_bonding_agg, con_bonding_cont, accept_card, desired_export_business, export_descrption, business_office) VALUES (%s, %s, %s, %s,\
+                        %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,\
+                        %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", (
+                            info['User ID:'],
+                            info['Name of Firm:'],
+                            info['Trade Name ("Doing Business As ..."):'],
+                            info['DUNS Number:'],
+                            info['Parent DUNS Number:'],
+                            info['Address, line 1:'],
+                            info['Address, line2:'],
+                            info['City:'],
+                            info['State:'],
+                            info['Zip:'],
+                            info['Phone Number:'],
+                            info['Fax Number:'],
+                            info['E-mail Address:'],
+                            info['WWW Page:'],
+                            info['E-Commerce Website:'],
+                            info['County Code (3 digit):'],
+                            info['Congressional District:'],
+                            info['Metropolitan Statistical Area:'],
+                            info['Cage Code:'],
+                            info['Year Established:'],
+                            info['GSA Advantage Contract(s):'],
+                            info['Ownership and Self-Certifications:'],
+                            info['SBA 8(a) Case Number:'],
+                            info['SBA 8(a) Entrance Date:'],
+                            info['SBA 8(a) Exit Date:'],
+                            info['HUBZone Certified?:'],
+                            info['HUBZone Certification Date:'],
+                            info['8(a) JV Entrance Date:'],
+                            info['8(a) JV Exit Date:'],
+                            naics_data,
+                            keywords,
+                            performance_json,
+                            list_id,
+                            info['Quality Assurance Standards'],
+                            info['Electronic Data Interchange capable?'],
+                            info['Export Business Activities'],
+                            info['Exporting to'],
+                            info['Service Bonding Level (aggregate)'],
+                            info['Service Bonding Level (per contract)'],
+                            info['Construction Bonding Level (aggregate)'],
+                            info['Construction Bonding Level (per contract)'],
+                            info['Accepts Government Credit Card?'],
+                            info['Desired Export Business Relationships:'],
+                            info['Description of Export Objective(s):'],
+                            office
+                        )
+                    )
+                    self.conn.commit()
+                except:
+                    print("Error %d: %s" % (e.args[0], e.args[1]))
+        except:
+            print "Error: unable to fecth profile data"
         return info
